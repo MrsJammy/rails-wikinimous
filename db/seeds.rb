34c52cea95c38043e9ff16b2aded456a9d03ff80
Sarah Jammernegg <sjammernegg@gmail.com>
2016-11-23 09:12:49 +0100

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  Article.create!(
+    title: Faker::Hipster.word,
+    content: Faker::Hipster.paragraph
+  )
+end
